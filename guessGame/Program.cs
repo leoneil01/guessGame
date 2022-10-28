40666fb5401800802644a6fe5db6d94d16e8aa76
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-10-28 11:23:18 +0800

    Add guessCount incrementation to increment the count each time the user guess

--- guessGame/Program.cs
+++ guessGame/Program.cs
@@ -3,12 +3,13 @@
     static void Main(string[]args)
     {
         string secretWord = "Hello", guess = "";
-        int gueesCount = 0;
+        int guessCount = 0;
 
         while (guess != secretWord)
         {
             Console.Write("Enter guess: ");
             guess = Console.ReadLine();
+            guessCount++;
         }
         Console.WriteLine("You win!");
     }
